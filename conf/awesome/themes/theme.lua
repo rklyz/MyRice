2ef209a2048c91899a9ceec9ee1338c68d133ce2
Neko <95261992+N3k0Ch4n@users.noreply.github.com>
2022-05-23 15:35:17 +0800

    Update theme.lua

--- conf/awesome/themes/theme.lua
+++ conf/awesome/themes/theme.lua
@@ -101,7 +101,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = walls_path .. "room.jpg"
+theme.wallpaper = gfs.get_configuration_dir() .. "/themes/mansion.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."layouts/fairhw.png"
